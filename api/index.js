3b879652d450c8592d046adbc5ae7f201c79bcb4
zidanwijayaaa7-cloud <zidanwijayaaa7@gmail.com>
2026-08-14 17:37:11 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // KONEKSI DATABASE MONGODB
-const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://admin:password123@cluster0.mongodb.net/racebox?retryWrites=true&w=majority";
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://ikanbuntal142_db_user:ZmRVHO3ko8MbqB53@cluster0.yye1dud.mongodb.net/?appName=Cluster0";
 
 if (mongoose.connection.readyState === 0) {
     mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
